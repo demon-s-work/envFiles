dd29317f69c70a4e4a3a5963a7308f3ac3bdeae8
Envy <envy1396@gmail.com>
2023-12-01 10:36:38 +0300

    added binaries, rename to envFiles

---
+++ bin/createMigrations.ps1
@@ -0,0 +1,60 @@
+﻿$migrationName = $args[0]
+$directory = Get-Location
+$directory = $directory.Path
+
+Write-Host Current location $directory
+
+class DbContext {
+    [string]$FolderPath
+    [string]$ContextName
+    DbContext([string]$path, [string]$name)
+    {
+        $this.FolderPath=$path
+        $this.ContextName=$name
+    }
+}
+
+$contexts = (
+[DbContext]::new("SqLiteMigrations", "SqLiteApplicationDbContext"),
+[DbContext]::new("MySqlMigrations","MySqlApplicationDbContext"),
+[DbContext]::new("MsSqlMigrations","MsSqlApplicationDbContext"),
+[DbContext]::new("PostgreMigrations","PostgreApplicationDbContext")
+)
+
+cp $directory\\CommonSettings.targets $directory\\Mapd.Server\
+cp $directory\\CommonSettings.targets $directory\\Mapd.Server.Dal\
+
+Write-Host "Build started" -ForegroundColor Yellow
+
+
+$buildResult = dotnet build $directory\\All.sln *>&1
+
+if($LASTEXITCODE -eq 0)
+{
+    Write-Host "Build success" -ForegroundColor Green
+}
+else
+{
+    $ErrorString = $buildResult -join [System.Environment]::NewLine
+}
+
+foreach($context in $contexts)
+{
+    $path = $context.FolderPath
+    $name = $context.ContextName
+    $error = ""
+    $migrationResult = dotnet ef migrations add --project $directory\Mapd.Server.Dal\Mapd.Server.Dal.csproj --startup-project $directory\Mapd.Server\Mapd.Server.csproj --context $name --configuration Debug --no-build $migrationName --output-dir Migrations\$path *> $error
+    
+    if($LASTEXITCODE -eq 0)
+    {
+        Write-Host "Migration for $name applyed"
+    }
+    else
+    {
+        Write-Host "Error with $name \n $error, stopping..."
+        break
+    }
+}
+
+rm $directory\Mapd.Server\CommonSettings.targets
+rm $directory\Mapd.Server.Dal\CommonSettings.targets
